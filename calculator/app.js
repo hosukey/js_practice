4025c736914cbdfb238076deaac121261217814b
hosu <hosulee11@gmail.com>
2021-10-26 22:44:21 -0700

    aadd userInput

--- calculator/app.js
+++ calculator/app.js
@@ -1,67 +1,21 @@
-// Questions:
-// calculator
-// add, subtract, times, divide numbers
-// clear all start from 0
-// able to input decimal numbers
+'use strict';
 
-// Sub-problems:
-// 1. input numbers by clicking on number pad (listen to calculator keys)
-// 2. get numbers to add to one line
-// 3. get signs to work (switch +,-,x,/ get parameters, first input & second input and calculate)
-// 4. display answer to the screen
-// 5. clear the calculator
-
-// Research
-
-// Pseudo-code
-
-const input = document.querySelector('.input');
-const calcKeys = document.querySelectorAll('#calculator_container');
+// Variables
+const calcButtons = document.querySelectorAll('button');
+const calcContainer = document.querySelector('#calculator_container');
 const display = document.querySelector('.input');
 
-calcKeys.forEach((e) => {
-  e.addEventListener('click', function (e) {
-    const key = e.target;
-    const action = key.dataset.action;
-    const keyContent = key.textContent;
-    const displayNum = display.textContent;
+let displayValue = 0;
 
-    //operators
-    if (
-      action === 'add' ||
-      action === 'subtract' ||
-      action === 'divide' ||
-      action === 'multiply'
-    ) {
-      console.log('operators');
-      if (action === 'add') {
-      }
-    }
+const userInput = function (e) {
+  const inputValue = e.target.textContent;
+  //display numbers to the display box
+  if (e.target.classList.contains('number')) {
+    displayValue += inputValue;
+    display.textContent = displayValue;
+    //how do I get rid of the 0 at the beginning?
+  }
+};
 
-    //decimal
-    if (action === 'decimal') {
-      if (display.textContent.includes('.')) {
-        return;
-      } else {
-        display.textContent = displayNum + '.';
-      }
-    }
-
-    //clear
-    if (action === 'clear') {
-      display.textContent = '0';
-    }
-
-    //equal key
-    if (action === 'equal') console.log('equal');
-
-    //display numbers
-    if (!action) {
-      if (displayNum === '0') {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayNum + keyContent;
-      }
-    }
-  });
-});
+// Event Handler
+calcContainer.addEventListener('click', userInput);
--- image-slider/app.js
+++ image-slider/app.js
@@ -8,85 +8,88 @@
 //overflow: hidden -> visible
 
 // Variables
-const sliderContainer = document.querySelector('.slider_container');
-const slides = document.querySelectorAll('.slide');
-const leftBtn = document.querySelector('.left');
-const rightBtn = document.querySelector('.right');
-const dotsContainer = document.querySelector('.dots_container');
+const slider = function () {
+  const sliderContainer = document.querySelector('.slider_container');
+  const slides = document.querySelectorAll('.slide');
+  const leftBtn = document.querySelector('.left');
+  const rightBtn = document.querySelector('.right');
+  const dotsContainer = document.querySelector('.dots_container');
 
-let curSlide = 0;
-const maxSlide = slides.length;
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
-// Functions
-const createDots = function () {
-  slides.forEach((_, i) => {
-    dotsContainer.insertAdjacentHTML(
-      'beforeend',
-      `<button class="dot" data-value=${i}></button>`
-    );
+  // Functions
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dotsContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dot" data-value=${i}></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dot')
+      .forEach((dot) => dot.classList.remove('dot_active'));
+    document
+      .querySelector(`.dot[data-value="${slide}"]`)
+      .classList.add('dot_active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+  };
+
+  const nextSlide = function () {
+    if (curSlide >= 0) {
+      curSlide++;
+    }
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    }
+    if (curSlide > 0) {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  function init() {
+    createDots();
+    goToSlide(0);
+    activateDot(0);
+  }
+  init();
+
+  //Event handlers
+
+  rightBtn.addEventListener('click', nextSlide);
+
+  leftBtn.addEventListener('click', prevSlide);
+
+  dotsContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dot')) {
+      const slide = e.target.dataset.value;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+
+  window.addEventListener('keydown', function (e) {
+    if (e.code === 'ArrowLeft') prevSlide();
+
+    if (e.code === 'ArrowRight') nextSlide();
   });
 };
-
-const activateDot = function (slide) {
-  document
-    .querySelectorAll('.dot')
-    .forEach((dot) => dot.classList.remove('dot_active'));
-  document
-    .querySelector(`.dot[data-value="${slide}"]`)
-    .classList.add('dot_active');
-};
-
-const goToSlide = function (slide) {
-  slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - slide)}%)`;
-  });
-};
-
-const nextSlide = function () {
-  if (curSlide >= 0) {
-    curSlide++;
-  }
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  }
-  goToSlide(curSlide);
-  activateDot(curSlide);
-};
-
-const prevSlide = function () {
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  }
-  if (curSlide > 0) {
-    curSlide--;
-  }
-  goToSlide(curSlide);
-  activateDot(curSlide);
-};
-
-function init() {
-  createDots();
-  goToSlide(0);
-  activateDot(0);
-}
-init();
-
-//Event handlers
-
-rightBtn.addEventListener('click', nextSlide);
-
-leftBtn.addEventListener('click', prevSlide);
-
-dotsContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('dot')) {
-    const slide = e.target.dataset.value;
-    goToSlide(slide);
-    activateDot(slide);
-  }
-});
-
-window.addEventListener('keydown', function (e) {
-  if (e.code === 'ArrowLeft') prevSlide();
-
-  if (e.code === 'ArrowRight') nextSlide();
-});
+slider();
