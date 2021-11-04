ffb7fdd3fcfc8dcdf7e36b9de53351199fefb3f8
hosu <hosulee11@gmail.com>
2021-11-03 23:02:11 -0700

    add number to a variable

--- calculator/app.js
+++ calculator/app.js
@@ -6,17 +6,39 @@ const calcContainer = document.querySelector('#calculator_container');
 const display = document.querySelector('.input');
 
 let displayValue = 0;
+let prevValue = 0;
+let newValue = 0;
 
 const userInput = function (e) {
   const inputValue = e.target.textContent;
   //displaying input numbers
-  if (e.target.classList.contains('number')) {
-    if (displayValue === 0) {
-      displayValue = inputValue;
-      display.textContent = displayValue;
-    } else {
-      displayValue += inputValue;
-      display.textContent = displayValue;
+  const inputDisplay = function () {
+    if (e.target.classList.contains('number')) {
+      if (displayValue === 0) {
+        displayValue = inputValue;
+        display.textContent = displayValue;
+      } else {
+        displayValue += inputValue;
+        display.textContent = displayValue;
+      }
+    }
+  };
+  inputDisplay();
+
+  //pseudo code - calculating:
+  if (e.target.classList.contains('sign')) {
+    //saves the first value
+    if (prevValue === 0) {
+      prevValue = displayValue;
+      console.log(`prevValue: ${prevValue}`);
+    }
+
+    if (e.target.classList.contains('plus')) {
+      displayValue = 0;
+      inputDisplay();
+      //How do I save newValue (2nd value) - WORK HERE!
+      newValue = displayValue;
+      console.log(`newValue: ${newValue}`);
     }
   }
 };
