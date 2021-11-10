2c16618febe7a768e63ec911daa7086d48a3dcf2
hosu <hosulee11@gmail.com>
2021-11-09 21:54:39 -0800

    add decimals and clear

--- calculator/app.js
+++ calculator/app.js
@@ -4,14 +4,24 @@
 const calcButtons = document.querySelectorAll('button');
 const calcContainer = document.querySelector('#calculator_container');
 const display = document.querySelector('.input');
+const acBtn = document.querySelector('.ac');
 
 let displayValue = 0;
 let prevValue = 0;
 let newValue = 0;
 
+// functions
+
+// Set display back to 0
+const resetDisplay = function () {
+  displayValue = 0;
+  display.textContent = displayValue;
+};
+
+// User input
 const userInput = function (e) {
   const inputValue = e.target.textContent;
-  //displaying input numbers
+  //display input
   const inputDisplay = function () {
     if (e.target.classList.contains('number')) {
       if (displayValue === 0) {
@@ -25,7 +35,7 @@ const userInput = function (e) {
   };
   inputDisplay();
 
-  //pseudo code - calculating:
+  //calculating using signs **** WORK HERE!
   if (e.target.classList.contains('sign')) {
     //saves the first value
     if (prevValue === 0) {
@@ -34,11 +44,33 @@ const userInput = function (e) {
     }
 
     if (e.target.classList.contains('plus')) {
+      //set displayValue back to 0
       displayValue = 0;
+      //let input show on display
       inputDisplay();
-      //How do I save newValue (2nd value) - WORK HERE!
-      newValue = displayValue;
+      //if there is prevValue save it to new value
+      if (prevValue) newValue = displayValue;
       console.log(`newValue: ${newValue}`);
+    }
+  }
+
+  // Clear (AC)
+  if (e.target.classList.contains('ac')) {
+    resetDisplay();
+  }
+
+  // Add decimal
+  //check if decimal exists, if not add it
+  if (e.target.classList.contains('dot')) {
+    const addingDecimal = function () {
+      displayValue += '.';
+      display.textContent = displayValue;
+    };
+    if (displayValue === 0) {
+      addingDecimal();
+    }
+    if (displayValue !== 0) {
+      if (!displayValue.includes('.')) addingDecimal();
     }
   }
 };
